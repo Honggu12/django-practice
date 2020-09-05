bef25637b49662dd5c3c35af218289729e8e1370
honggu12 <gu4734@hanmail.net>
2020-09-05 23:35:25 +0900

    guestbook application finish

--- django_practices/urls.py
+++ django_practices/urls.py
@@ -1,5 +1,4 @@
 """django_practices URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -18,7 +17,7 @@ from django.urls import path
 
 import helloworld.views as helloworldviews
 import emaillist.views as emaillistviews
-#import guestbook.views as guestbookviews
+import guestbook.views as guestbookviews
 
 urlpatterns = [
     path('hello/', helloworldviews.hello),
@@ -29,10 +28,10 @@ urlpatterns = [
     path('emaillist/form', emaillistviews.form),
     path('emaillist/add', emaillistviews.add),
 
-    #path('guestbook/', guestbookviews.index),
-    #path('guestbook/add', guestbookviews.add),
-    #path('guestbook/deleteform', guestbookviews.deleteform),
-    #path('guestbook/delete', guestbookviews.delete),
+    path('guestbook/', guestbookviews.index),
+    path('guestbook/add', guestbookviews.add),
+    path('guestbook/deleteform', guestbookviews.deleteform),
+    path('guestbook/delete', guestbookviews.delete),
 
     path('admin/', admin.site.urls),
 ]
--- emaillist/tests.py
+++ emaillist/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+# from django.test import TestCase  (Django TDD)
+import guestbook.models as guestbookmodel
 
-# Create your tests here.
+
+def test_guestbookmodel_insert():
+    guestbookmodel.insert('안대혁', '1234', '안녕하세요~')
+
+
+def test_guestbookmodel_fetchlist():
+    results = guestbookmodel.fetchlist()
+    print(results)
+
+
+def test_guestbookmodel_delete():
+    guestbookmodel.delete(6, '1234')
+
+
+# test_guestbookmodel_insert()
+# test_guestbookmodel_delete()
+test_guestbookmodel_fetchlist()
